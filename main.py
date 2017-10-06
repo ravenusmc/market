c5954afdf466c252a56aed456b0109a49446ce5f
ravenusmc <mcuddy77@gmail.com>
2017-10-06 15:19:14 -0400

    added log in page

---
+++ main.py
@@ -0,0 +1,30 @@
+#importing outside libraries for use in the project
+from flask import Flask, session, jsonify, redirect, url_for, escape, render_template, request, flash
+
+#Setting up Flask
+app = Flask(__name__)
+
+#This route takes the user to the landing page
+@app.route('/')
+def landing():
+  if request.method == 'POST':
+    print('POST')
+    #Recieving the information from the user.
+    username = request.form['username']
+    password = request.form['password']
+    print(username)
+  return render_template('login.html')
+
+#This route takes the user to the signup page
+@app.route('/signup')
+def signup():
+  return render_template('signup.html')
+
+
+
+# set the secret key. keep this really secret:
+app.secret_key = 'n3A\xef(\xb0Cf^\xda\xf7\x97\xb1x\x8e\x94\xd5r\xe0\x11\x88\x1b\xb9'
+
+#This line will actually run the app.
+if __name__ == '__main__':
+    app.run(debug=True)
