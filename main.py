56ff33cdecb4b542fc819431e2806b0123d177b8
ravenusmc <mcuddy77@gmail.com>
2017-10-23 20:40:59 -0400

    getting data to javascript code

--- main.py
+++ main.py
@@ -95,12 +95,10 @@ def stats():
     total_pounds = food.get_poundage(food_data)
     #I turn the two lists, foods and total_pounds into a dictionary
     pound_dictionary = dict(zip(foods, total_pounds))
-
-    #poundData = json.dumps(pound_dictionary, ensure_ascii=False)
-
+    # print(poundData)
     total_profit = food.get_profit(food_data)
     profit_dictionary = dict(zip(foods, total_profit))
-    return render_template('stats.html', pounds=pound_dictionary, profit=profit_dictionary)
+    return render_template('stats.html', pounds=pound_dictionary, profit=profit_dictionary, pound_data=json.dumps(pound_dictionary))
 
 
 #This function is what will log out the user.
